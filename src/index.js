71196e5454a8ba0e58036afca604265eeafb3f78
ederpbj <ederpbj@gmail.com>
2021-06-03 12:35:40 -0300

    iniciando o projeto

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />, //mostra o app dentro do root, que é o projeto react no arquivo App.js, funções retornaod html
+  document.getElementById('root') //vai no root e busca o elemento pelo id
+);
